2b7d6d005b07146d14e60ac9e94abd209cc51839
Joffrey LAURENT <jojo58400@hotmail.fr>
2018-06-20 15:09:31 +0200

    create chain

---
+++ create_chain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chain_name=chain1
+port=1234
+
+multichain-util create $chain_name -default-network-port=$port
+sed -i -e "s/anyone-can-connect = false/anyone-can-connect = true /g" ~/.multichain/$chain_name/params.dat
+sed -i -e "s/anyone-can-create = false/anyone-can-create = true /g" ~/.multichain/$chain_name/params.dat
+multichaind $chain_name -daemon
+
+firewall-cmd --permanent --zone=public --add-port=$port/tcp
+systemctl restart firewalld.service
