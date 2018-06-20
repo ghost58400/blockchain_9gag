5bd040b279968983baf26dd980970d1f245846f1
Joffrey LAURENT <jojo58400@hotmail.fr>
2018-06-20 23:27:00 +0200

    ajout persistance

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 dhclient
+echo dhclient >> /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
+echo ip a >> ~/.bashrc
 yum update -y
 yum install -y nano wget ntpdate vim-common
 ntpdate pool.ntp.org
@@ -11,3 +14,4 @@ wget https://www.multichain.com/download/multichain-1.0.5.tar.gz
 tar -xvzf multichain-1.0.5.tar.gz
 cd multichain-1.0.5
 mv multichaind multichain-cli multichain-util /usr/bin/
+ip a
