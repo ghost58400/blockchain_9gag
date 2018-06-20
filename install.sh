3c2319df8351b9cfa1c67963032e145e63e4966f
Joffrey LAURENT <jojo58400@hotmail.fr>
2018-06-20 14:41:02 +0200

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dhclient
+yum update
+yum install -y nano wget htop ntpdate
+ntpdate pool.ntp.org
+
+cd /tmp
+wget https://www.multichain.com/download/multichain-1.0.5.tar.gz
+tar -xvzf multichain-1.0.5.tar.gz
+cd multichain-1.0.5
+mv multichaind multichain-cli multichain-util /usr/bin/
+
