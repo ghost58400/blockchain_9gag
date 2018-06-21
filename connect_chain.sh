3f3a6ae1e4cc11c8173763fef74586940571d9ce
Joffrey LAURENT <jojo58400@hotmail.fr>
2018-06-21 09:56:57 +0200

    envoi sans sauvegarder la privkey

--- connect_chain.sh
+++ connect_chain.sh
@@ -9,6 +9,7 @@ sleep 2
 rm -rf ~/.multichain/$chain_name
 
 multichaind $chain_name@$ip:$port -daemon -autosubscribe=streams
+sleep 2
 
 json_myaddr=$(multichain-cli $chain_name getaddresses)
 json_addr=$(multichain-cli $chain_name liststreamkeyitems default_account address)
@@ -21,7 +22,11 @@ my_addr=$(echo $json_myaddr | python -c "import sys, json; print json.load(sys.s
 default_privkey=$(echo $hex_priv | xxd -p -r)
 default_address=$(echo $hex_addr | xxd -p -r)
 
+#multichain-cli $chain_name importprivkey $default_privkey
+#multichain-cli $chain_name sendfrom $default_address $my_addr 0
 
-#faire une seule ligne en raw sans ajouter privkey au wallet
-multichain-cli $chain_name importprivkey $default_privkey
-multichain-cli $chain_name sendfrom $default_address $my_addr 0
+multichain-cli $chain_name importaddress $default_address
+txid=$(multichain-cli $chain_name createrawsendfrom $default_address {\"$my_addr\":0})
+signed_hex_json=$(multichain-cli $chain_name signrawtransaction $txid null \[\"$default_privkey\"])
+signed_hex=$(echo $signed_hex_json | python -c "import sys, json; print json.load(sys.stdin)['hex']")
+multichain-cli $chain_name sendrawtransaction $signed_hex
