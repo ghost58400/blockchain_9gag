94ef4e2529ef440ac72899c952245f745e1ad873
Coquerant Logan <logan.coquerant@insa-cvl.fr>
2018-06-21 11:52:55 +0200

    flask init jsp

---
+++ web/app.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import json
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+	return "Welcome to out reddit style app based on the multichain !"
+	# streams = récupération streams
+	# return jsonify(streams)
+
+
+@app.route('/new_post', methods=['POST'])
+def new_post():
+	return "Créer un post"
+	
+	# return jsonify(stream)
+
+
+@app.route('/show_post/<num_stream>', methods=['GET'])
+def show_post(num_stream):
+	return "Post : {}".format(num_stream)
+
+	# stream = récupération stream
+	# return jsonify(stream)
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=8080)
+
+
