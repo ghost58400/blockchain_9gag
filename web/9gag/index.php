596733755c2c608aa47c01fd6cb8283cf3cabb62
Coquerant Logan <logan.coquerant@insa-cvl.fr>
2018-06-22 10:08:36 +0200

    form create post + forme index.php

--- web/9gag/index.php
+++ web/9gag/index.php
@@ -13,52 +13,48 @@
 <a href="#" class="sponsor g"></a>
 <a href="#" class="sponsor d"></a>
 <!-- fin sponsor -->
-<main>
-<div id="top-nav">
-	<div class="main-nav">
-		<h1><a href="/">Memes.com : all your memes</a></h1>
-	    <div class="menu-items">
-			<ul>
-			<li><a href="/news/">best</a></li><li><a href="/" class="current">what's hot</a></li>
-		    
-		</ul>
-		</div><div class="user-functions">
-		<ul>
-        	<li><a href="/random" class="random-meme" title="randomize">random memes</a></li>
-			<li><a href="/submit" class="upload-meme"><span>upload a funny</span></a></li>
-            <li><a href="/caption" class="make-meme"><span>caption a meme</span></a></li>
-		</ul>
-	    </div>
-	    <div class="nav-search">
-		<form id="search" name="search" action="/search" method="GET" role="search" class="navbar-search">		  
-			<input id="searchwords" placeholder="search..." class="search-query" name="searchwords" type="text">
-		</form>
-	    </div>
-	    <div class="menu-items user-login">
-			<ul>
-			<li id="shownsfw"><a href="javascript:void(0)" onclick="toggleNSFW();" id="shownsfwlink">show NSFW</a></li>
-			<li id="fblogin"><a href="javascript:void(0)" onclick="doLogin();" id="fbloginlink">login</a><a href="javascript:void(0)" id="fblogintext" style="display:none;"></a></li>
-		</ul>
-		</div>
-	    <div class="clear"></div>
-	</div>
+<div class="container">
+    <div id="top-nav">
+        <div class="main-nav">
+            <h1><a href="/">Memes.com : all your memes</a></h1>
+            <div class="menu-items">
+                <ul>
+                    <li><a href="news/">best</a></li>
+                    <li><a href="/" class="current">what's hot</a></li>
+
+                </ul>
+            </div>
+            <div class="user-functions">
+                <ul>
+                    <li><a href="random/" class="random-meme" title="randomize">random memes</a></li>
+                    <li><a href="submit/" class="upload-meme"><span>upload a funny</span></a></li>
+                    <li><a href="caption/" class="make-meme"><span>caption a meme</span></a></li>
+                    <li><a href="create_post.html"><span>Create a post</span></a></li>
+                </ul>
+            </div>
+            <div class="nav-search">
+                <form id="search" name="search" action="search/" method="GET" role="search" class="navbar-search">
+                    <input id="searchwords" placeholder="search..." class="search-query" name="searchwords" type="text">
+                </form>
+            </div>
+            <div class="menu-items user-login">
+                <ul>
+                    <li id="shownsfw"><a href="javascript:void(0)" onclick="toggleNSFW();" id="shownsfwlink">show
+                            NSFW</a></li>
+                    <li id="fblogin"><a href="javascript:void(0)" onclick="doLogin();" id="fbloginlink">login</a><a
+                                href="javascript:void(0)" id="fblogintext" style="display:none;"></a></li>
+                </ul>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <!-- end top nav bar-->
+    <!-- begin main content-->
+
+    <div ng-view></div>
 </div>
-	<!-- end top nav bar-->
-	<!-- begin main content-->
-<div id="container">
-		<div class="page-content">
-		<div class="post-wrap">
 
-				<div id="posts">
 
-	
-</main>
-<div id="header"></div> 
-
-<div ng-controller="TaskController">
-
-    <!-- // Modal -->
-</div>
 
 <!-- Jquery JS file -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
