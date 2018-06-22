c51b1de1b5603eade2c3e4ac15b6cceaac2b804c
Coquerant Logan <logan.coquerant@insa-cvl.fr>
2018-06-22 10:30:42 +0200

    initialisation angular + redirection

---
+++ web/9gag/app.js
@@ -0,0 +1,12 @@
+
+angular.module('App', ['ngRoute','App.post',]).
+
+//routage
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/home'});
+
+}]);
+
