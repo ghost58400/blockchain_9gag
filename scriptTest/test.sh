1f356930e6cfea6d15793f5b273864c920403e00
Unknown <energieadx5@gmail.com>
2018-06-25 16:36:17 +0200

    correction

--- scriptTest/test.sh
+++ scriptTest/test.sh
@@ -5,6 +5,6 @@ npm install web3
 echo "Enter <ip>:<port> of Etherum-VM"
 read ip
 sed -i -e "s/localhost:8545*/$ip/g" index.js
-sed -i -e "s/localhost:8545*/$ip/g" index.js
+sed -i -e "s/localhost:8545*/$ip/g" deploy.js
 python html2.py
 python -m SimpleHTTPServer 1337
