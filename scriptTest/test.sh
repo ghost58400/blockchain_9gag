a99ddc389289cd13a969103935362172431df073
Unknown <energieadx5@gmail.com>
2018-06-25 16:22:35 +0200

    change

---
+++ scriptTest/test.sh
@@ -0,0 +1,10 @@
+npm install
+npm install webpack
+npm install solc
+npm install web3
+echo "Enter <ip>:<port> of Etherum-VM"
+read ip
+sed -i -e "s/localhost:8545*/$ip/g" index.js
+sed -i -e "s/localhost:8545*/$ip/g" index.js
+python html2.py
+python -m SimpleHTTPServer 1337
