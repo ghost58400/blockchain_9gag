a1145c34688c2be0e06d29f2891f0c55231b408a
Coquerant Logan <logan.coquerant@insa-cvl.fr>
2018-06-25 11:33:30 +0200

    functions angular

--- web/9gag/post/post.js
+++ web/9gag/post/post.js
@@ -22,10 +22,48 @@ angular.module('App.post', ['ngRoute'])
     .controller('PostController', function ($scope, $http) {
 
         $scope.list_posts = [];
+        $scope.current_post = null;
 
+        $http.get('/get_posts')
+            .then(function success(e) {
+                $scope.errors = [];
+                if (e.data !== '')
+                    $scope.list_posts = e.data;
+            }, function error(e) {
+                $scope.errors = e.data.errors;
+            });
+
+        $scope.show_post = function (title) {
+            var found = $scope.list_posts.find(function(element) {
+                return element.title === title;
+            });
+            if (found) {
+                $scope.current_post = found;
+            }
+        };
+
+        $scope.upvote = function (title) {
+            var found = $scope.list_posts.find(function(element) {
+                return element.title === title;
+            });
+            if (found) {
+                $scope.current_post = found;
+            //    upvote
+            }
+        };
+
+        $scope.downvote = function (title) {
+            var found = $scope.list_posts.find(function(element) {
+                return element.title === title;
+            });
+            if (found) {
+                $scope.current_post = found;
+             //    downvote
+            }
+        }
     })
 
-    .controller('LoginController', function ($scope, $htpp) {
+    .controller('LoginController', function ($scope, $http) {
         $scope.list_user = [];
         $scope.local_user = null;
     });
