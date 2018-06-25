02c863041b8ecd164016246b1b2fe08ea7d567d8
Coquerant Logan <logan.coquerant@insa-cvl.fr>
2018-06-25 17:03:39 +0200

    connect chain

--- web/static/chain/chain.js
+++ web/static/chain/chain.js
@@ -6,20 +6,23 @@ angular.module('App.chain', ['ngRoute'])
         $scope.port_chain = 1234;
         $scope.name_chain = null;
         $scope.connected = false;
+        $scope.nickname_chain = null;
 
         // $http.get('/connect/chain1/172.17.0.10/1234');
 
         $scope.connectToChain = function() {
-            $http.get('/connect/'+$scope.name_chain+'/'+$scope.ip_chain+'/'+$scope.port_chain)
-                .then(function success(e) {
-                    $scope.errors = [];
-                    if (e.data !== '')
-                        console.log("/connect");
+            if ( $scope.ip_chain !== null && $scope.name_chain !== null && $scope.nickname_chain) {
+                $http.get('/connect/' + String($scope.name_chain) + '/' + String($scope.ip_chain) + '/' + String($scope.port_chain)+'/'+String($scope.nickname_chain))
+                    .then(function success(e) {
+                        $scope.errors = [];
+                        if (e.data !== '')
+                            console.log("/connect");
                         $scope.connected = true;
-                    console.log(e.data);
-                }, function error(e) {
-                    console.log("error");
-                    $scope.errors = e.data.errors;
-                });
+                        console.log(e.data);
+                    }, function error(e) {
+                        console.log("error");
+                        $scope.errors = e.data.errors;
+                    });
+            }
         }
     });
